f1bb6f66341920e6cba4b6fa2592e81639b39a40
= <mdhvkothari007@gmail.com>
2019-01-17 22:44:10 +0530

    c

--- data_in_pandas.py
+++ data_in_pandas.py
@@ -1,12 +1,13 @@
 import pandas as pd
 from pandas import DataFrame , read_csv
-name = ['madhav','rahul','rishab','bikesh']
-age = [18,16,18,22]
+name = ['madhav','rahul','rishab','bikesh','nitin','vaibhav']
+age = [18,16,18,22,10,25]
+place=['jewar','mathura','bihar','bihar','noida','bharatpur']
 #we can zip these arrya into one
-datalist = list(zip(name,age))
+datalist = list(zip(name,age,place))
 print(datalist)
 #now we can convert these data into DataFrame
-df = pd.DataFrame(data = datalist,columns=['Names','Ages'])
+df = pd.DataFrame(data = datalist,columns=['Names','Ages','Place'])
 print(df)
 #now we can convert this data into csv file
 df.to_csv('datafile.csv',index=False)
@@ -27,3 +28,16 @@ sorted = df.sort_values(['Ages'],ascending=False)
 print(sorted.head(1))
 #another method for finding the max value
 print(df['Ages'].max())
+#we can change the index
+df = df.set_index('Names')
+print(df.head(2))
+print(df['Place'][:2])
+
+print(df.describe())
+
+#how to select the row
+print(df.iloc[0:2])
+#we can use boolian values
+print(df[df["Ages"]>18])
+#it will give the number of the people present in the data
+print((df["Ages"]>18).sum())
