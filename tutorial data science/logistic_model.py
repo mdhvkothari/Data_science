2bc6ab67049a30b7474daa41edc6af5bc3404579
mdhv <mdhvkothari007@gmail.com>
2019-05-08 13:32:00 +0530

    commit

--- tutorial data science/logistic_model.py
+++ tutorial data science/logistic_model.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri May  3 14:50:44 2019
+Created on Fri MaY  3 14:50:44 2019
 
 @author: mdhvk
 """
@@ -31,18 +31,49 @@ classifier.fit(X_train,Y_train)
 Y_pred = classifier.predict(X_test)
 
 
-#Now we make confusion matrix to evaluate the  accuracy of a classification
+#Now we make confusion matrix to evaluate the  accuracY of a classification
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(Y_test,Y_pred)
 #this will give output like [[65,  3],[ 8, 24]] which means that our model predict 65+24 correct prediction
 #and 3+8 are incorrect
 
 #Visulation the trainig set
+from matplotlib.colors import ListedColormap
+X_set, Y_set = X_train, Y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(Y_set)):
+    plt.scatter(X_set[Y_set == j, 0], X_set[Y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated SalarY')
+plt.legend()
+plt.show()
 
 
+#Visulatoion of test set
 
-
-
+from matplotlib.colors import ListedColormap
+X_set, Y_set = X_test, Y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(Y_set)):
+    plt.scatter(X_set[Y_set == j, 0], X_set[Y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Train set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated SalarY')
+plt.legend()
+plt.show()
 
 
 
